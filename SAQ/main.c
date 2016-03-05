4ce4113a8052dc217b422fc391448920bb86ca3a
한유진 <scarlett@han-yujin-ui-MacBook-Air.local>
2016-03-05 18:50:14 +0900

    saq 1st

--- SAQ/main.c
+++ SAQ/main.c
@@ -6,10 +6,39 @@
 //  Copyright © 2016년 한유진. All rights reserved.
 //
 
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, const char * argv[]) {
+int main() {
     // insert code here...
+    int integerArray[10] = {0};
+    FILE *pFile = NULL;
+    pFile = fopen("/Users/scarlett/Documents/input/input1.txt", "r");
+    
+    if (pFile != NULL)
+    {
+        char strTemp[255];
+        char *pStr;
+        char *p;
+        int column = 0;
+        int integerConverted = 0;
+        
+        pStr = fgets(strTemp, sizeof(strTemp), pFile);
+        
+        p = strtok(strTemp, " ");
+        for(column = 0; column < 10; column++)
+        {
+            if(!p)
+             break;
+            
+            integerConverted = atoi(p);
+            integerArray[column] = integerConverted;
+            p = strtok(NULL, " ");
+            printf("%d \n", integerArray[column]);
+        }
+    }
     printf("Hello, World!\n");
     return 0;
 }
