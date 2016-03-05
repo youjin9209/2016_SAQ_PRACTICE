2144a59a1cbfa63c727d0e922ec93d75b611e1bd
youjin9209 <youjin9200@naver.com>
2016-03-05 19:26:28 +0900

    Create saq_1_1.c

---
+++ saq_1_1.c
@@ -0,0 +1,68 @@
+/* 1장
+ * 1절 선택정렬
+ * 입력받은 10개의 성적에 따라 낮은 점수를 받은 학생부터 순차적으로 학점을 F, D, D+, C, C+, B, B+, A, A+(2명) 으로 부여.
+ * 입력된 정수를 오름차순으로 정렬하고, 각 정수 밑에 학점을 출력한다.
+ * 세번째 학생의 학점을 출력한다
+ */
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    // insert code here...
+    int integerArray[10] = {0};
+    char scoreArray[10] = {0};
+    
+    int min = 0;
+    int i = 0, j = 0, nTmp = 0;
+    
+    FILE *pFile = NULL;
+    pFile = fopen("/Users/scarlett/Documents/input/input1.txt", "r");
+    
+    if (pFile != NULL)
+    {
+        char strTemp[255];
+        char *pStr;
+        char *p;
+        int column = 0;
+        int integerConverted = 0;
+        
+        pStr = fgets(strTemp, sizeof(strTemp), pFile);
+        
+        p = strtok(strTemp, " ");
+        for(column = 0; column < 10; column++)
+        {
+            if(!p)
+             break;
+            
+            integerConverted = atoi(p);
+            integerArray[column] = integerConverted;
+            p = strtok(NULL, " ");
+        }
+    }
+    
+    for(i = 0; i < 9; i++)
+    {
+        min = i;
+        for(j = i+1; j < 10; j++)
+        {
+            if(integerArray[j] < integerArray[min])
+                min = j;
+        }
+        nTmp = integerArray[min];
+        integerArray[min] = integerArray[i];
+        integerArray[i] = nTmp;
+    }
+    
+    for(int w = 0; w < 10; w++)
+    {
+        printf("%d ", integerArray[w]);
+    }
+    
+    for(int p = 0; p < 10; p++)
+    {
+    
+    }
+    return 0;
+}
