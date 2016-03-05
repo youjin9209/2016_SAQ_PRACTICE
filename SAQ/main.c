004cd784fc6e3da1af6a81ff79272a32ca241648
한유진 <scarlett@han-yujin-ui-macbook-air.local>
2016-03-05 18:09:53 +0900

    Initial Commit

---
+++ SAQ/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SAQ
+//
+//  Created by 한유진 on 2016. 3. 5..
+//  Copyright © 2016년 한유진. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
